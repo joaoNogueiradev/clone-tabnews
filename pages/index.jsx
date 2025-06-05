58d875dbb281e91f485bc88281826aaf58603c73
João Nogueira <147201184+joaoNogueiradev@users.noreply.github.com>
2025-06-05 03:01:16 +0000

    adiciona os arquivos  e

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-    return <h1>Eu te amo! Se você também me ama dê uma palminha e me dê um beijo!</h1>
+  return (
+    <h1>Eu te amo! Se você também me ama dê uma palminha e me dê um beijo!</h1>
+  );
+};
+
+function teste() {
+  console.log(teste);
 }
 
-export default Home
+function teste2() {
+  console.log("indentação errada");
+}
+
+export default Home;
